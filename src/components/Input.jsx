30e09b9b0c92638cff1b15026a449f9af19127a7
Samir Mulla <samirmulla710@gmail.com>
2024-08-18 20:35:30 +0530

    component-style : outline color changed

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,7 +3,10 @@ import React from "react";
 const Input = ({ id, name, type, label, value, onChange, error, pattern }) => {
   return (
     <div className="mb-4">
-      <label htmlFor={id} className="block text-gray-700 font-bold mb-2 text-lg">
+      <label
+        htmlFor={id}
+        className="block text-gray-700 font-bold mb-2 text-lg"
+      >
         {label}
       </label>
       <input
@@ -15,7 +18,7 @@ const Input = ({ id, name, type, label, value, onChange, error, pattern }) => {
         pattern={pattern}
         className={`w-full px-3 py-2 border ${
           error ? "border-red-500" : "border-gray-300"
-        } rounded focus:outline-none focus:border-blue-500`}
+        } rounded focus:outline-none focus:border-lime-600`}
       />
       {error && <p className="text-red-500 text-sm">{error}</p>}
     </div>
